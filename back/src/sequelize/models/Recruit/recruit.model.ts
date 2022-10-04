ae98efc0559081159a1ab23198911121a75e780b
ivy <jihyunivy@gmailcom>
2022-10-04 18:11:39 +0900

    [DB] FIX: add @Column annotation

--- back/src/sequelize/models/Recruit/recruit.model.ts
+++ back/src/sequelize/models/Recruit/recruit.model.ts
@@ -10,46 +10,46 @@ import { AllowNull, AutoIncrement, Column, DataType, Model, PrimaryKey, Table } 
 class Recruit extends Model {
     @PrimaryKey
     @AutoIncrement
+    @AllowNull(false)
     @Column({
         type: DataType.INTEGER,
         comment:'채용 공고에 대한 식별자'
     })
-    @AllowNull(false)
 	r_id: number;
 
+    @AllowNull(false)
     @Column({
         type: DataType.INTEGER,
         comment:'공고를 업로드 한 회사에 대한 식별자'
     })
-    @AllowNull(false)
 	c_id: number;
 
+    @AllowNull(false)
     @Column({
         type: DataType.INTEGER,
         comment:'채용 보상금'
     })
-    @AllowNull(false)
 	r_signing_bounus: number;
 
+    @AllowNull(false)
     @Column({
         type: DataType.STRING,
         comment:'채용 포지션'
     })
-    @AllowNull(false)
 	r_position: string;
 
+    @AllowNull(false)
     @Column({
         type: DataType.STRING,
         comment:'채용 내용'
     })
-    @AllowNull(false)
 	r_description: string;
 
+    @AllowNull(false)
     @Column({
         type: DataType.STRING,
         comment:'사용 기술'
     })
-    @AllowNull(false)
 	r_tech_stack: string;
 }
 
